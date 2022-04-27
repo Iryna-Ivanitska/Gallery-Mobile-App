93ae7f09d00ca5cb48a33f3eec5793dd65afc0d8
Iryna Ivanytska <ira.pechenitsya@gmal.com>
2022-04-27 11:25:32 +0300

    add navigation

---
+++ navigation.js
@@ -0,0 +1,31 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import Favorite from './pages/Favorite';
+import Pictures from './pages/Pictures';
+
+const Tab = createBottomTabNavigator();
+
+export default function Tabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+        screenOptions={{
+          tabBarLabelPosition: 'beside-icon'
+        }}
+        >
+        <Tab.Screen 
+          name="Pictures" 
+          component={Pictures} 
+          options={{
+            tabBarIcon:() => <SimpleLineIcons name="picture" size={24} color="black" />
+          }}
+          />
+        <Tab.Screen name="Favorite pictures" component={Favorite} 
+          options={{
+            tabBarIcon:() => <SimpleLineIcons name="heart" size={24} color="black" />
+          }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ pages/Favorite.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Favorite() {
+  return (
+    <View >
+      <Text>Favourite!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
