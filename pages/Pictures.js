ecf36c23484d5d96bf49a483b120bfa7636f0a16
Iryna Ivanytska <ira.pechenitsya@gmal.com>
2022-04-29 17:39:19 +0300

    clean code

--- pages/Pictures.js
+++ pages/Pictures.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { StyleSheet, FlatList } from "react-native";
 import { connect } from "react-redux";
 import PictureItem from "../components/PictureItem";
 import Search from "../components/search";
-import { favoriteToggle, getData, getPictures } from "../redux/Actions";
+import { favoriteToggle, getPictures } from "../redux/Actions";
 
 const Pictures = (props) => {
 
@@ -34,7 +34,6 @@ const mapStateToProps = (state) => state;
 const mapDispatchToProps = (dispatch) => {
   return {
     favoriteToggle: (img) => dispatch(favoriteToggle(img)),
-    getData: (data) => dispatch(getData(data)),
   };
 };
 
--- redux/store.js
+++ redux/store.js
@@ -13,7 +13,7 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  whitelist: ['pictures']
+  // whitelist: ['pictures']
 };
 
 const persistedReducer = persistReducer(persistConfig, PictureReducer)
